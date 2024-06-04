38f2890482e943caaad9eea2a511112ed5313a38
Rakesh <rakeshrakshit740@gmail.com>
2024-06-04 10:40:11 +0530

    authentication middleare  created

---
+++ api/middleware_test.go
@@ -0,0 +1,15 @@
+package api
+
+// func Test_authMiddleware(t *testing.T) {
+// 	tests := []struct {
+// 		name string
+// 		setupAuth func ()  {}
+// 	}{
+// 		// TODO: Add test cases.
+// 	}
+// 	for _, tt := range tests {
+// 		t.Run(tt.name, func(t *testing.T) {
+// 			authMiddleware()
+// 		})
+// 	}
+// }
